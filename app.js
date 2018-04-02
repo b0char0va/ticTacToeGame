d08b46ea282a900c92ac4d1f9c41a504a4e7e104
Mona <bocharova.mona@gmail.com>
2018-04-02 20:41:12 +0100

    working prototype

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var game;
 var symbol;
 var compSymbol;
-var array = [];
-var centerMoveArray = [1, 3, 7, 9];
-var clicks = 0;
+var freecells = 9;
 
+var m = [
+    [-1, -1, -1],
+    [-1, -1, -1],
+    [-1, -1, -1]
+];
 
 $(document).ready(function () {
     $("#one").click(function () {
@@ -30,7 +33,8 @@ $(document).ready(function () {
     });
     $(".XO").click(function () {
         symbol = $(this).text();
-        $(".table").removeClass("hide");
+
+        $(".table").removeClass("disabled hide");
         $(".score").removeClass("hide");
         if (game === 1) {
             $("#scoreComp").removeClass("hide");
@@ -44,7 +48,18 @@ $(document).ready(function () {
         $("#back").addClass("hide");
     });
     $("#Reset").click(function () {
+        m = [
+            [-1, -1, -1],
+            [-1, -1, -1],
+            [-1, -1, -1]
+        ];
+        freecells = 9;
+        $("#result").empty();
+        $("#result").addClass("hide");
+        $("#win").addClass("hide");
+        $("#draw").addClass("hide");
         $(".cell").empty();
+        $(".cell").removeClass("disabledCell");
         $(".table").addClass("hide");
         $(".score").addClass("hide");
         $("#score2").addClass("hide");
@@ -56,164 +71,125 @@ $(document).ready(function () {
         $("#back").addClass("hide");
     });
     $(".cell").click(function () {
-        clicks++;
+        var comp;
+        var opponent;
+        freecells--;
+        if (symbol === "X") {
+            opponent = 1;      // if player chooses to play with X, player is 1
+            comp = 0;            //computer is "O" and O is 0
+            compSymbol = "O";
+        } else {
+            opponent = 0;       //if player chooses to play with O, player is 0
+            comp = 1;             //comptuer is "X" and X is 1
+            compSymbol = "X";
+        }
         $(this).html(symbol);
-        var cellId = $(this).attr("id");
-        array.push(cellId);
-        console.log(array);
-        play(cellId);
+        $(this).addClass("disabledCell");
+        var cellId = parseInt($(this).attr("id"));
+        playerMatrixMove(cellId, opponent, comp, compSymbol);
     })
 });
 
-function play(id) {
-
-    if (symbol === "X") {
-        compSymbol = "O";
-    } else {
-        compSymbol = "X";
+function analyzeMoves(m, player, depth) {
+    if (checkWinner(m, +!player)) {
+        return {score: player === 1 ? 10 - depth : depth - 10}
     }
-    if (clicks === 1 && id === "5") {
-        var compMove = centerMoveArray[Math.floor((Math.random() * 3))];
-        if (compMove !== id) {
-            $("#" + compMove).html(compSymbol);
-            array.push(compMove);
+
+    var possibleMoves = [];
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            // is cell i,j empty?
+            if (m[i][j] !== -1) {
+                continue;
+            }
+            m[i][j] = player;
+            var stats = analyzeMoves(m, +!player, depth + 1);
+            possibleMoves.push({i: i, j: j, score: stats.score});
+            m[i][j] = -1;
         }
-        return;
-    } else if (clicks === 1 && id !== "5") {
-        $("#5").html(compSymbol);
-        array.push("5");
-        return;
     }
 
-    if ((($("#1").text() === symbol && $("#2").text() === symbol) || ($("#1").text() === compSymbol && $("#2").text() === compSymbol)) && $("#3").text() === "") {
-        $("#3").html(compSymbol);
-        array.push("3");
-        return;
+    if (possibleMoves.length === 0) {
+        return {score: 0}
     }
-    if ((($("#2").text() === symbol && $("#3").text() === symbol) || ($("#2").text() === compSymbol && $("#3").text() === compSymbol)) && $("#1").text() === "") {
-        $("#1").html(compSymbol);
-        array.push("1");
-        return;
-    }
-    if ((($("#4").text() === symbol && $("#5").text() === symbol) || ($("#4").text() === compSymbol && $("#5").text() === compSymbol)) && $("#6").text() === "") {
-        $("#6").html(compSymbol);
-        array.push("6");
-        return;
-    }
-    if ((($("#5").text() === symbol && $("#6").text() === symbol) || ($("#5").text() === compSymbol && $("#6").text() === compSymbol)) && $("#4").text() === "") {
-        $("#4").html(compSymbol);
-        array.push("4");
-        return;
-    }
-    if ((($("#7").text() === symbol && $("#8").text() === symbol) || ($("#7").text() === compSymbol && $("#8").text() === compSymbol)) && $("#9").text() === "") {
-        $("#9").html(compSymbol);
-        array.push("9");
-        return;
-    }
-    if ((($("#8").text() === symbol && $("#9").text() === symbol) || ($("#8").text() === compSymbol && $("#9").text() === compSymbol)) && $("#7").text() === "") {
-        $("#7").html(compSymbol);
-        array.push("7");
-        return;
-    }
-    if ((($("#1").text() === symbol && $("#4").text() === symbol) || ($("#1").text() === compSymbol && $("#4").text() === compSymbol)) && $("#7").text() === "") {
-        $("#7").html(compSymbol);
-        array.push("7");
-        return;
-    }
-    if ((($("#4").text() === symbol && $("#7").text() === symbol) || ($("#4").text() === compSymbol && $("#7").text() === compSymbol)) && $("#1").text() === "") {
-        $("#1").html(compSymbol);
-        array.push("1");
-        return;
-    }
-    if ((($("#2").text() === symbol && $("#5").text() === symbol) || ($("#2").text() === compSymbol && $("#5").text() === compSymbol)) && $("#8").text() === "") {
-        $("#8").html(compSymbol);
-        array.push("8");
-        return;
-    }
-    if ((($("#5").text() === symbol && $("#8").text() === symbol) || ($("#5").text() === compSymbol && $("#8").text() === compSymbol)) && $("#2").text() === "") {
-        $("#2").html(compSymbol);
-        array.push("2");
-        return;
-    }
-    if ((($("#3").text() === symbol && $("#6").text() === symbol) || ($("#3").text() === compSymbol && $("#6").text() === compSymbol)) && $("#9").text() === "") {
-        $("#9").html(compSymbol);
-        array.push("9");
-        return;
-    }
-    if ((($("#6").text() === symbol && $("#9").text() === symbol) || ($("#6").text() === compSymbol && $("#9").text() === compSymbol)) && $("#3").text() === "") {
-        $("#3").html(compSymbol);
-        array.push("3");
-        return;
-    }
-    if ((($("#1").text() === symbol && $("#5").text() === symbol) || ($("#1").text() === compSymbol && $("#5").text() === compSymbol)) && $("#9").text() === "") {
-        $("#9").html(compSymbol);
-        array.push("9");
-        return;
-    }
-    if ((($("#5").text() === symbol && $("#9").text() === symbol) || ($("#5").text() === compSymbol && $("#9").text() === compSymbol)) && $("#1").text() === "") {
-        $("#1").html(compSymbol);
-        array.push("1");
-        return;
-    }
-    if ((($("#3").text() === symbol && $("#5").text() === symbol) || ($("#3").text() === compSymbol && $("#5").text() === compSymbol)) && $("#7").text() === "") {
-        $("#7").html(compSymbol);
-        array.push("7");
-        return;
-    }
-    if ((($("#5").text() === symbol && $("#7").text() === symbol) || ($("#5").text() === compSymbol && $("#7").text() === compSymbol)) && $("#3").text() === "") {
-        $("#3").html(compSymbol);
-        array.push("3");
-        return;
-    }
-    if ((($("#1").text() === symbol && $("#3").text() === symbol) || ($("#1").text() === compSymbol && $("#3").text() === compSymbol)) && $("#2").text() === "") {
-        $("#2").html(compSymbol);
-        array.push("2");
-        return;
-    }
-    if ((($("#4").text() === symbol && $("#6").text() === symbol) || ($("#4").text() === compSymbol && $("#6").text() === compSymbol)) && $("#5").text() === "") {
-        $("#5").html(compSymbol);
-        array.push("5");
-        return;
-    }
-    if ((($("#7").text() === symbol && $("#9").text() === symbol) || ($("#7").text() === compSymbol && $("#9").text() === compSymbol)) && $("#8").text() === "") {
-        $("#8").html(compSymbol);
-        array.push("8");
-        return;
-    }
-    if ((($("#1").text() === symbol && $("#7").text() === symbol) || ($("#1").text() === compSymbol && $("#7").text() === compSymbol)) && $("#4").text() === "") {
-        $("#4").html(compSymbol);
-        array.push("4");
-        return;
-    }
-    if ((($("#2").text() === symbol && $("#8").text() === symbol) || ($("#2").text() === compSymbol && $("#8").text() === compSymbol)) && $("#5").text() === "") {
-        $("#5").html(compSymbol);
-        array.push("5");
-        return;
-    }
-    if ((($("#3").text() === symbol && $("#9").text() === symbol) || ($("#3").text() === compSymbol && $("#9").text() === compSymbol)) && $("#6").text() === "") {
-        $("#6").html(compSymbol);
-        array.push("6");
-        return;
-    }
-    if ((($("#1").text() === symbol && $("#9").text() === symbol) || ($("#1").text() === compSymbol && $("#9").text() === compSymbol)) && $("#5").text() === "") {
-        $("#5").html(compSymbol);
-        array.push("5");
-        return;
-    }
-    if ((($("#3").text() === symbol && $("#7").text() === symbol) || ($("#3").text() === compSymbol && $("#7").text() === compSymbol)) && $("#5").text() === "") {
-        $("#5").html(compSymbol);
-        array.push("5");
-        return;
-    }
-    randomMove();
-}
 
-function randomMove() {
-    var compMove = centerMoveArray[Math.ceil((Math.random() * 8))];
-    if (!array.includes(compMove)) {
-        $("#" + compMove).html(compSymbol);
-        console.log(compMove);
+    var bestMove, l;
+    if (player === 0) {
+        var max = Number.NEGATIVE_INFINITY;
+        for (l in possibleMoves) {
+            if (possibleMoves[l].score > max) {
+                bestMove = possibleMoves[l];
+                max = possibleMoves[l].score;
+            }
+        }
     } else {
-        randomMove();
+        var min = Number.POSITIVE_INFINITY;
+        for (l in possibleMoves) {
+            if (possibleMoves[l].score < min) {
+                bestMove = possibleMoves[l];
+                min = possibleMoves[l].score;
+            }
+        }
+    }
+    return bestMove;
+}
+
+function checkWinner(m, p) {
+    for (var i = 0; i < 3; i++) {
+        if (m[i][0] === p && m[i][1] === p && m[i][2] === p) {
+            return true;
+        }
+        if (m[0][i] === p && m[1][i] === p && m[2][i] === p) {
+            return true;
+        }
+    }
+
+    if (m[0][0] === p && m[1][1] === p && m[2][2] === p) {
+        return true;
+    }
+
+    if (m[2][0] === p && m[1][1] === p && m[0][2] === p) {
+        return true;
+    }
+
+    return false;
+}
+
+function playerMatrixMove(cellId, opponent, comp, compSymbol) {
+    var i = Math.floor((cellId - 1) / 3);
+    var j = Math.floor((cellId - 1) % 3);
+    m[i][j] = opponent;
+    compMatrixMove(m, opponent, comp, compSymbol); //passing updated matrix, computer's playing symbol value, and number of free cells
+}
+
+function compMatrixMove(m, opponent, player, compSymbol) {
+    if (checkWinner(m, opponent)) {
+        $("#result").removeClass("hide");
+        $("#result").text("You won!!");
+        $(".table").addClass("disabled");
+    } else if (freecells === 0 && !playerWin) {
+        $("#result").removeClass("hide");
+        $("#result").text("It's draw!!");
+        $(".table").addClass("disabled");
+    } else {
+        var gameStats = analyzeMoves(m, player, 0);
+        var i = gameStats.i;
+        var j = gameStats.j;
+        var cellId = (i * 3) + 1 + j;
+        m[i][j] = player;
+        $("#" + cellId).text(compSymbol);
+        $("#" + cellId).addClass("disabledCell");
+        freecells--;
+        var playerWin = checkWinner(m, player);
+        if (checkWinner(m, player)) {
+            $("#result").removeClass("hide");
+            $("#result").text("You lost!!");
+            $(".table").addClass("disabled");
+        } else if (freecells === 0 && !playerWin) {
+            $("#result").removeClass("hide");
+            $("#result").text("It's draw!!");
+            $(".table").addClass("disabled");
+        }
     }
 }
+
